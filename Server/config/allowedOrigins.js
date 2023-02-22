e7e8f6ad073f7523c1d5584c8b7dd5deed7fc56a
Luigi Cardone <lcardone22@gmail.com>
2023-02-22 19:51:13 +0100

    stable version

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,8 +1,11 @@
 const allowedOrigins = [
-    'https://tua-car.onrender.com',
+    'https://new.leads.tua-car.it/',
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    'http://localhost:8000'
+    'http://localhost:8000',
+    'https://3.75.158.163:8000',
+    'https://3.125.183.140:8000',
+    'https://35.157.117.28:8000'
 ]
 
 export default allowedOrigins
